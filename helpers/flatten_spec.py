c24feee9faf76e9dd13cfa9a83ab919eaeb07a69
DrJuneMoone <96371033+MooneDrJune@users.noreply.github.com>
2022-08-30 19:25:39 +0530

    Add files via upload

---
+++ helpers/flatten_spec.py
@@ -0,0 +1,15 @@
+import unittest
+
+from tswdtf.helpers.flatten import flatten
+
+
+class FlattenSpec(unittest.TestCase):
+    def test_flatten(self):
+        x = flatten(
+            (
+                1,
+                {"a": (1, "z"), "b": {"c": "d"}},
+            )
+        )
+
+        assert x == [1, 1, "z", "d"] or x == [1, "d", 1, "z"]
