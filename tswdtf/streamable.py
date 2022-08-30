eca1799fb275cf7aecd6ae05d2d848f4b0ea2548
DrJuneMoone <96371033+MooneDrJune@users.noreply.github.com>
2022-08-30 19:41:44 +0530

    Add files via upload

---
+++ tswdtf/streamable.py
@@ -0,0 +1,144 @@
+import tensorflow as tf
+import functools
+
+from tswdtf.helpers.map_fn import map_fn
+from tswdtf.helpers.flatten import flatten
+
+
+class Streamable:
+    @staticmethod
+    def check_types_equality(type1, type2, error_message):
+        """
+        Check the types are equal or throw otherwise.
+        """
+        if type1 != type2:
+            raise Exception(error_message % (str(type1), str(type2)))
+        return
+
+    @staticmethod
+    def to_type(values):
+        return map_fn(
+            values,
+            [values],
+            lambda val: val.dtype
+            if isinstance(val, tf.Tensor)
+            else tf.convert_to_tensor(val).dtype,
+        )
+
+    def call_streamed(self, inputs_tensors, provided_state):
+        """
+        Stream inputs_tensors into the operator. The variable provided_state can
+        be None if initial state is not provided by the user or it can be a compatible
+        initial state provided by the user.
+        """
+
+        inputs_sizes = tuple(map(lambda x: tf.shape(x)[0], flatten(inputs_tensors)))
+        size = inputs_sizes[0]
+        same_size = functools.reduce(
+            lambda acc, x: tf.logical_and(acc, tf.equal(x, size)),
+            inputs_sizes,
+            tf.constant(True),
+        )
+
+        assert_same_size = tf.assert_equal(
+            same_size,
+            tf.constant(True),
+            data=inputs_sizes,
+            message="inputs have different sizes.",
+        )
+
+        inputs_0 = map_fn(inputs_tensors, [inputs_tensors], lambda x: x[0])
+        outputs_0, state, initial_state = self.forward_step(inputs_0, provided_state)
+
+        # if state is provided, we take it as the initial value.
+        if not provided_state is None:
+            initial_state = provided_state
+
+        def cond(i, loop_inputs, loop_state, outputs):
+            return i < size
+
+        def loop(i, loop_inputs, loop_state, loop_outputs):
+            inputs_i = map_fn(loop_inputs, [loop_inputs], lambda x: x[i])
+
+            outputs_i, next_state, _ = self.forward_step(inputs_i, loop_state)
+
+            Streamable.check_types_equality(
+                Streamable.to_type(loop_state),
+                Streamable.to_type(next_state),
+                "Provided state and model state has different types (%s != %s).",
+            )
+
+            new_outputs = map_fn(
+                outputs_i, [outputs_i, loop_outputs], lambda x, y: y.write(i, x)
+            )
+            map_fn(
+                loop_state,
+                [loop_state, next_state],
+                lambda x, y: tf.convert_to_tensor(y).set_shape(
+                    tf.convert_to_tensor(x).get_shape()
+                ),
+            )
+            return (i + 1, loop_inputs, next_state, new_outputs)
+
+        i0 = tf.constant(0)
+        outputs = map_fn(
+            outputs_0, [outputs_0], lambda x: tf.TensorArray(dtype=x.dtype, size=size)
+        )
+
+        with tf.control_dependencies([assert_same_size]):
+            i_f, inputs_f, state_f, outputs_f = tf.while_loop(
+                cond,
+                loop,
+                loop_vars=[i0, inputs_tensors, initial_state, outputs],
+                name="stream_loop",
+            )
+
+        outputs = map_fn(outputs_f, [outputs_f], lambda o: o.stack())
+        return (outputs, state_f, initial_state)
+
+    def __call__(self, inputs, state=None, streamable=True):
+        # Preprocessing to convert python literals into tensors.
+        inputs = map_fn(inputs, [inputs], lambda x: tf.convert_to_tensor(x))
+        if not state is None:
+            state = map_fn(state, [state], lambda x: tf.convert_to_tensor(x))
+
+        if streamable:
+            return self.call_streamed(inputs, state)
+        else:
+            return self.forward_step(inputs, state)
+
+    def step(self, *inputs):
+        """
+        Where the internals of the operator must be implemented.
+        """
+        raise Exception("Not implemented.")
+
+    def forward_step(self, inputs, state):
+        if not isinstance(inputs, (tuple, list)):
+            inputs = (inputs,)
+        if state is None:
+            state = ()
+        elif not isinstance(state, (tuple, list)):
+            state = (state,)
+        # By default we unpack inputs and state.
+        return self.step(*inputs, *state)
+
+    def concat(self, v1, v2):
+        """
+        Helper function concatenating two tensors together.
+        This is used for concatenating shapes together.
+        """
+        if v1 == ():
+            return v2
+        elif v2 == ():
+            return v1
+        return tf.concat([v1, v2], axis=0)
+
+
+class MetaStreamable(Streamable):
+    def forward_step(self, inputs, state):
+        # For a meta streamable, the operator does not know
+        # the content of input and state as it can be anything.
+        # Therefore, we simply pass the values or tuples to the
+        # operator implementation.
+        return self.step(inputs, state)
---
+++ tswdtf/streamable_spec.py
@@ -0,0 +1,317 @@
+import unittest
+import tensorflow as tf
+
+from tswdtf.streamable import Streamable, flatten
+
+
+class Simple(Streamable):
+    def step(self, value):
+        return value * tf.constant(2), (), ()
+
+
+class SimpleSpec(unittest.TestCase):
+    def test_simple_step(self):
+        sim = Simple()
+        x = tf.constant(4)
+        y, _, _ = sim(x, streamable=False)
+
+        with tf.Session() as sess:
+            output = sess.run(y)
+
+        self.assertEqual(output, 8)
+
+    def test_streamable_simple_value(self):
+        sim = Simple()
+        x = tf.constant([0, 1, 2, 3, 4, 5])
+        y, _, _ = sim(x)
+
+        with tf.Session() as sess:
+            output = sess.run(y)
+
+        self.assertEqual(output.tolist(), [0, 2, 4, 6, 8, 10])
+
+
+class MultiDim(Streamable):
+    def step(self, value):
+        return value * tf.constant([2, 3]), (), ()
+
+
+class MultiDimSpec(unittest.TestCase):
+    def test_streamable_multidim(self):
+        multi_dim = MultiDim()
+        x = tf.constant([[0, 0], [1, 1], [2, 2], [3, 3], [4, 4], [5, 5]])
+        y, _, _ = multi_dim(x)
+
+        with tf.Session() as sess:
+            output = sess.run(y)
+
+        self.assertEqual(
+            output.tolist(), [[0, 0], [2, 3], [4, 6], [6, 9], [8, 12], [10, 15]]
+        )
+
+
+class Tuple(Streamable):
+    def step(self, value1, value2):
+        return (value1 * 2, value2 * 3), (), ()
+
+
+class TupleSpec(unittest.TestCase):
+    def test_streamable_tuple(self):
+        op = Tuple()
+        x = tf.constant([0, 1, 2, 3, 4, 5])
+        y = tf.constant([1, 2, 3, 4, 5, 6])
+        (z0, z1), _, _ = op(inputs=(x, y))
+
+        with tf.Session() as sess:
+            output = sess.run([z0, z1])
+
+        self.assertEqual(output[0].tolist(), [0, 2, 4, 6, 8, 10])
+        self.assertEqual(output[1].tolist(), [3, 6, 9, 12, 15, 18])
+
+
+class List(Streamable):
+    def step(self, value1, value2):
+        return [value1 * 2, value2 * 3], (), ()
+
+
+class ListSpec(unittest.TestCase):
+    def test_streamable_list(self):
+        op_list = List()
+        x = tf.constant([0, 1, 2, 3, 4, 5])
+        y = tf.constant([1, 2, 3, 4, 5, 6])
+        z, _, _ = op_list(inputs=(x, y))
+
+        with tf.Session() as sess:
+            output = sess.run(z)
+
+        self.assertEqual(output[0].tolist(), [0, 2, 4, 6, 8, 10])
+        self.assertEqual(output[1].tolist(), [3, 6, 9, 12, 15, 18])
+
+
+class Dict(Streamable):
+    def step(self, value):
+        return {"val1": value["val1"] * 2, "val2": value["val2"] * 3}, (), ()
+
+
+class DictSpec(unittest.TestCase):
+    def test_streamable_dict(self):
+        op = Dict()
+        x = tf.constant([0, 1, 2, 3, 4, 5])
+        y = tf.constant([1, 2, 3, 4, 5, 6])
+        t, _, _ = op({"val1": x, "val2": y})
+
+        with tf.Session() as sess:
+            output = sess.run(t)
+
+        self.assertEqual(output["val1"].tolist(), [0, 2, 4, 6, 8, 10])
+        self.assertEqual(output["val2"].tolist(), [3, 6, 9, 12, 15, 18])
+
+
+class Nested(Streamable):
+    def step(self, value):
+        x = value["val1"][0]
+        y = value["val1"][1]["a"][0]
+        z = value["val2"]
+        return {"val1": (x * 2, {"a": [y * 3]}), "val2": z * 3}, (), ()
+
+
+class NestedSpec(unittest.TestCase):
+    def test_streamable_nested(self):
+        op = Nested()
+        x = tf.constant([0, 1, 2, 3, 4, 5])
+        y = tf.constant([1, 2, 3, 4, 5, 6])
+        z = tf.constant([2, 3, 4, 5, 6, 7])
+        t, _, _ = op({"val1": (x, {"a": [y]}), "val2": z})
+
+        with tf.Session() as sess:
+            output = sess.run(t)
+
+        self.assertEqual(output["val1"][0].tolist(), [0, 2, 4, 6, 8, 10])
+        self.assertEqual(output["val1"][1]["a"][0].tolist(), [3, 6, 9, 12, 15, 18])
+        self.assertEqual(output["val2"].tolist(), [6, 9, 12, 15, 18, 21])
+
+
+class WithState(Streamable):
+    def step(self, value, iteration=None):
+        if iteration is None:
+            iteration = tf.zeros(dtype=value.dtype, shape=tf.shape(value))
+        return value * 2, iteration + 2, iteration
+
+
+class WithStateSpec(unittest.TestCase):
+    def test_streamable_step_with_state_not_provided(self):
+        ws = WithState()
+        x = tf.constant(4)
+        y = ws(x, streamable=False)
+        with tf.Session() as sess:
+            output = sess.run(y)
+
+        self.assertEqual(output[0], 8)
+        self.assertEqual(output[1], 2)
+
+    def test_streamable_step_with_state_provided(self):
+        ws = WithState()
+        x = tf.constant(4)
+        y = ws(x, state=tf.constant(5), streamable=False)
+        with tf.Session() as sess:
+            output = sess.run(y)
+
+        self.assertEqual(output[0], 8)
+        self.assertEqual(output[1], 7)
+
+    def test_streamable_with_provided_state(self):
+        op_with_state = WithState()
+        x = tf.constant([0, 1, 2, 3, 4, 5])
+        i = tf.constant(5)
+        t, j, _ = op_with_state(x, state=i)
+
+        with tf.Session() as sess:
+            output = sess.run([t, j])
+
+        self.assertEqual(output[0].tolist(), [0, 2, 4, 6, 8, 10])
+        self.assertEqual(output[1], 17)
+
+    def test_streamable_with_not_provided_state(self):
+        op = WithState()
+        x = tf.constant([0, 1, 2, 3, 4, 5])
+        t, j, _ = op(x)
+
+        with tf.Session() as sess:
+            output = sess.run([t, j])
+
+        self.assertEqual(output[0].tolist(), [0, 2, 4, 6, 8, 10])
+        self.assertEqual(output[1], 12)
+
+
+class Complex(Streamable):
+    def step(self, value1, value2, state1=None, state2=None):
+        if state1 is None:
+            state1 = tf.constant(0)
+        if state2 is None:
+            state2 = {"b": tf.zeros(2), "c": tf.constant(5)}
+
+        new_state2 = {"b": state2["b"] + tf.ones(2), "c": state2["c"] - 1}
+        new_value2 = {"a": value2 * 3}
+
+        return (value1 * 2, new_value2), (state1 + 1, new_state2), (state1, state2)
+
+
+class ComplexSpec(unittest.TestCase):
+    def test_streamable_complex(self):
+        op = Complex()
+        x = tf.constant([0, 1, 2, 3, 4, 5])
+        y = tf.constant([0, 1, 2, 3, 4, 5])
+
+        z = op(inputs=(x, y))
+
+        with tf.Session() as sess:
+            output, state, init_state = sess.run(z)
+
+        self.assertEqual(output[0].tolist(), [0, 2, 4, 6, 8, 10])
+        self.assertEqual(output[1]["a"].tolist(), [0, 3, 6, 9, 12, 15])
+        self.assertEqual(state[0], 6)
+        self.assertEqual(state[1]["b"].tolist(), [6, 6])
+        self.assertEqual(state[1]["c"].tolist(), -1)
+
+
+class NestedOperator(Streamable):
+    def __init__(self):
+        super().__init__()
+        self.with_state = WithState()
+
+    def step(self, value, prev_state=None):
+        new_value, next_state, init_state = self.with_state(
+            value, prev_state, streamable=False
+        )
+        return new_value + 1, next_state, init_state
+
+
+class NestedOpSpec(unittest.TestCase):
+    def test_streamable_nested_operator(self):
+        op = NestedOperator()
+        x = tf.constant([0, 1, 2, 3, 4, 5])
+        z, s, _ = op(x)
+
+        with tf.Session() as sess:
+            output = sess.run([z, s])
+
+        self.assertEqual(output[0].tolist(), [1, 3, 5, 7, 9, 11])
+        self.assertEqual(output[1], 12)
+
+
+class MultiNestedOperators(Streamable):
+    def __init__(self):
+        self.with_state = WithState()
+
+    def step(self, value, iteration=None, prev_state1=None, prev_state2=None):
+        if iteration is None:
+            iteration = tf.constant(0)
+
+        new_value1, next_state1, init_state1 = self.with_state(
+            value, prev_state1, streamable=False
+        )
+        new_value2, next_state2, init_state2 = self.with_state(
+            new_value1, prev_state2, streamable=False
+        )
+        return (
+            new_value2 + 1,
+            (iteration + 1, next_state1, next_state2),
+            (iteration, init_state1, init_state2),
+        )
+
+
+class MultiNestedOperatorsSpec(unittest.TestCase):
+    def test_streamable_multi_nested_operators(self):
+        op = MultiNestedOperators()
+        x = tf.constant([0, 1, 2, 3, 4, 5])
+        z, s, _ = op(x)
+
+        with tf.Session() as sess:
+            output = sess.run([z, s])
+
+        self.assertEqual(output[0].tolist(), [1, 5, 9, 13, 17, 21])
+        self.assertEqual(output[1], (6, 12, 12))
+
+
+class BadInputStateOperator(Streamable):
+    def step(self, value, iteration=None):
+        return value, (), tf.constant(0)
+
+
+class BadOutputStateOperator(Streamable):
+    def step(self, value):
+        return value, value, ()
+
+
+class StreamableSpec(unittest.TestCase):
+    def test_streamable_bad_input_sizes(self):
+        op = Tuple()
+        x = tf.constant([0, 1, 2, 3, 4, 5])
+        y = tf.constant([1, 2, 3, 4, 5, 6, 7])
+        z, _, _ = op(inputs=(x, y))
+
+        with self.assertRaises(tf.errors.InvalidArgumentError) as context:
+            with tf.Session() as sess:
+                output = sess.run(z)
+
+    def test_streamable_bad_input_state_operator(self):
+        op = BadInputStateOperator()
+        x = tf.constant([0, 1, 2, 3, 4, 5])
+        with self.assertRaises(Exception) as context:
+            z, s, _ = op(x, state=tf.constant(0))
+
+        self.assertEqual(
+            str(context.exception),
+            "Provided state and model state has different types (<dtype: 'int32'> != ()).",
+        )
+
+    def test_streamable_bad_output_state_operator(self):
+        op = BadOutputStateOperator()
+        x = tf.constant([0, 1, 2, 3, 4, 5])
+        with self.assertRaises(Exception) as context:
+            z, s, _ = op(x)
+
+        self.assertEqual(
+            str(context.exception),
+            "Provided state and model state has different types (() != <dtype: 'int32'>).",
+        )
